15e02c9892675b36556669cddb706e036573eaf9
Ubiratã <100245139+ubirataantunes@users.noreply.github.com>
2022-06-08 20:11:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = document.getElementById("password"), confirm_password = document.getElementById("password-confirmation");
+
+function validatePassword(){
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Senhas diferentes!");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
